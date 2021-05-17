fc3f7de6baf442a7b07499928ba0e48bf5a7e583
sebazero <sebastian.seguel1@mail.udp.cl>
2021-05-16 21:15:08 -0400

    Proyecto Terminado

--- app/src/main/java/com/example/magicball/MainActivity.java
+++ app/src/main/java/com/example/magicball/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -9,25 +10,28 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.myapplication.R.id;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    //referencia la imagen
+
     private ImageButton Ball;
     private TextView respuesta;
-    private String [] respuestaArray ={"Tu dia sera genial", "Tranquilo todo estara bien", "Sigue participando"};
+    private final String [] respuestaArray ={"Tu dia sera genial", "Tranquilo todo estara bien", "Sigue participando","Alcanzaras tus metas"};
 
 
-    @SuppressLint("WrongViewCast")
-    @Override
+
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //referencia la imagen
         Ball = findViewById(R.id.btn);
         //referencia de texto
-        respuesta = findViewById(R.id.respuestvista);
+        respuesta = findViewById(R.id.respuestavista);
         //Proceso
         Ball.setOnClickListener(this);
         Toast.makeText(MainActivity.this, "consulta tu suerte", Toast.LENGTH_SHORT).show();
@@ -42,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 respuesta.setText(respuestaArray[rand]);
 
                 break;
+
         }
     }
 
